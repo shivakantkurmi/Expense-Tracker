564c7a4f46cbfb22f807f6497eb646f34d22446f
shivakant kurmi <shivakantkurmi49@gmail.com>
2025-06-18 22:10:29 +0530

    Installed Dependencies

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import{
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
 const App = () =>{
     return(
-        <div className="text-3xl text-purple-600">App</div>
+        <div>
+
+        </div>
     )
 }
 
