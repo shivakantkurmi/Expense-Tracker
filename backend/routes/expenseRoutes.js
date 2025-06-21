48ba0bb97a8135fb2833960c7eed149c629d45bb
aribah123armin <eleenaarmin@gmail.com>
2025-06-21 19:53:21 +0530

    Create expenseRoutes.js

---
+++ backend/routes/expenseRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getExpenses,
+  addExpense,
+  updateExpense,
+  deleteExpense,
+} = require("../controllers/expenseController");
+
+const auth = require("../middleware/authMiddleware");
+
+router.use(auth);
+
+router.get("/", getExpenses);
+router.post("/", addExpense);
+router.put("/:id", updateExpense);
+router.delete("/:id", deleteExpense);
+
+module.exports = router;
