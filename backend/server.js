0585ec34d87d794ab6ef63ca2315ed29d9a8e052
shivakantkurmi <shivakantkurmi49@gmail.com>
2025-06-24 23:00:27 +0530

    DB Connected

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const connectDB = require("./config/db");
+const connectDB=require("./config/db");
+
 dotenv.config();
 const app = express();
 app.use(cors());
@@ -12,12 +13,12 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/expenses", require("./routes/expenseRoutes"));
 
+connectDB();
+
 // DB Connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error(err));
-
-connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
