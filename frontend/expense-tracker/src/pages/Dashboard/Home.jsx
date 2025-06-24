fd500f130bd77410e5a014033464fe85e902b37c
shivakantkurmi <shivakantkurmi49@gmail.com>
2025-06-24 19:39:18 +0530

    files

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 const Home = () =>{
     return(
-        <div>Home</div>
-    )
-}
+        <DashboardLayout activeMenu="dashoard">
+            <div className="my-5 mx-auto">  </div>
+            </DashboardLayout>
+
+    );
+};
 
 export default Home
