324e865163c8addc70566d264215e9c7542e627a
shivakantkurmi <shivakantkurmi49@gmail.com>
2025-06-30 05:20:54 +0530

    Frontend Connected to server deployed on backend

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://expense-tracker-backend-86bzi5xbz-shivakant-kurmis-projects.vercel.app";
 
 export const API_PATHS = {
   AUTH: {
