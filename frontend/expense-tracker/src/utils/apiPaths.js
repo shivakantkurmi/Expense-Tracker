176dfcb40cbe8655ce3f5f17e2532e6000dc2de6
shivakantkurmi <shivakantkurmi49@gmail.com>
2025-06-30 06:35:55 +0530

    retry

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://expense-tracker-backend-topaz.vercel.app";
+export const BASE_URL = "https://expense-tracker-snowy-gamma.vercel.app";
 
 export const API_PATHS = {
   AUTH: {
